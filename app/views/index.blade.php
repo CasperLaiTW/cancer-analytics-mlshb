cb5ecb39d085c4cc352556b2a5e62a4dd7fdae6b
Casper Lai <casper.lai@sleepingdesign.com>
2015-01-12 22:36:56 +0800

    Update input name.

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -3,7 +3,7 @@
         <h1>分析名單</h1>
         {{ Form::open(['route' => ['analytics'], 'files' => true]) }}
             <div class="form-group">
-                {{ Form::file('file') }}
+                {{ Form::file('source') }}
                 <p class="help-block">請選擇名單</p>
             </div>
             <button type="submit" class="btn btn-default">開始分析</button>
