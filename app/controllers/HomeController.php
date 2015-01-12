3f27e54fc2826a0c13f8714651d0cc62285cef53
Casper Lai <casper.lai@sleepingdesign.com>
2015-01-13 00:35:00 +0800

    Add process analytics.

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -14,7 +14,23 @@ class HomeController extends BaseController
         if (Input::hasFile('source')) {
             $file = Input::file('source');
             $path = $file->getRealPath();
-            dd($path);
+            $first = [];
+            $nonCheck = [];
+            Excel::load($path, function ($render) use (&$first, &$nonCheck) {
+                $rows = $render->toArray();
+                foreach ($rows as $row) {
+                    $keys = array_keys($row);
+                    $area = preg_split('/(　|\s)?\d+/', $row[$keys[6]]);
+                    if ($row[$keys[0]] === 'V') {
+                        $first[$area[0]] = (empty($first[$area[0]]) ? 0 : $first[$area[0]]) + 1;
+                    } elseif ($row[$keys[1]] === 'V') {
+                        $nonCheck[$area[0]] = (empty($nonCheck[$area[0]]) ? 0 : $nonCheck[$area[0]]) + 1;
+                    }
+                }
+            });
+            var_dump($first);
+            dd($nonCheck);
         }
+
     }
 }
